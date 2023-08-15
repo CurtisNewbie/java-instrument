6c911803bf5c7497695a2c8f8d952f119f27362d
yongj.zhuang <yongj.zhuang@outlook.com>
2023-08-15 13:49:18 +0800

    Modified:  perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java

--- perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java
+++ perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java
@@ -2,8 +2,10 @@ package com.curtisnewbie.agent.perf;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
@@ -17,11 +19,22 @@ public class PerfAgent {
     public static void premain(String arguments, Instrumentation instrumentation) {
         new AgentBuilder.Default()
                 .type(not(nameStartsWith("java."))
+                        .and(not(isSynthetic()))
+                        .and(not(nameStartsWith("org.slf4j")))
                         .and(not(nameStartsWith("net.bytebuddy")))
                         .and(not(nameStartsWith("org.springframework.util")))
                         .and(not(nameStartsWith("sun.reflect")))
-                        .and(not(isSynthetic()))
-                        .and(not(nameContains("logging"))))
+                        .and(not(nameStartsWith("com.sun")))
+                        .and(not(nameContains("logging")))
+                        .and(not(nameContains("BeanDefinition")))
+                        .and(not(nameContains("ResolvableType")))
+                        .and(not(nameStartsWith("cn.hutool")))
+                        .and(not(nameStartsWith("org.yaml.snakeyaml")))
+                        .and(not(nameStartsWith("com.fasterxml")))
+                        .and(not(nameStartsWith("org.springframework.core.env")))
+                        .and(not(nameStartsWith("org.springframework.core.convert")))
+                        .and(not(nameStartsWith("org.springframework.boot.context.properties")))
+                )
                 .transform((builder, type, classLoader, module) ->
                         builder.visit(timingAdvice.on(isPublic()
                                 .and(not(isAbstract()))
@@ -31,25 +44,44 @@ public class PerfAgent {
                 .installOn(instrumentation);
     }
 
-    static class TimingAdvice {
+    public static class TimingAdvice {
 
-        static final long UNIT = 1000_000L;
-        static final long THRESHOLD = 500_000_000L;
+        public static ThreadLocal<Integer> depth = ThreadLocal.withInitial(() -> 0);
+        public static final long UNIT = 1000_000L;
+        public static final long THRESHOLD = 1_000_000_000L;
+//        static final long THRESHOLD = 0L;
 
         @Advice.OnMethodEnter
         static long enter() {
+            if (!Thread.currentThread().getName().equalsIgnoreCase("main"))
+                return 0L;
+
+            depth.set(depth.get() + 1);
             return System.nanoTime();
         }
 
         @Advice.OnMethodExit
-        static void exit(@Advice.Enter long time, @Advice.Origin String origin) {
+        static void exit(@Advice.Enter long time, @Advice.Origin String origin, @Advice.AllArguments(typing = Assigner.Typing.DYNAMIC) Object[] args) {
             if (!Thread.currentThread().getName().equalsIgnoreCase("main"))
                 return;
 
             long took = (System.nanoTime() - time);
-            if (took > THRESHOLD) {
-                System.out.printf("Perf, %s, %s, ms\n", origin, took / UNIT);
+            if (took >= THRESHOLD) {
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i] != null && !isPrimitive(args[i].getClass())) {
+                        args[i] = args[i].getClass().getName();
+                    }
+                }
+                System.out.printf("Perf, %d, %s %s, %d, ms\n", depth.get(), origin, Arrays.toString(args), took / UNIT);
             }
+            depth.set(depth.get() - 1);
+        }
+
+        public static boolean isPrimitive(Class<?> type) {
+            return type.isPrimitive() ||
+                    type == Double.class || type == Float.class || type == Long.class ||
+                    type == Integer.class || type == Short.class || type == Character.class ||
+                    type == Byte.class || type == Boolean.class || type == String.class;
         }
     }
 }

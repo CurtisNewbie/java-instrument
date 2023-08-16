256a814a1876f2a52443037dc3be1164e214f0c7
yongj.zhuang <yongj.zhuang@outlook.com>
2023-08-16 13:04:58 +0800

    Deleted:   perf-agent/.gitignore Modified:  perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java

--- perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java
+++ perf-agent/src/main/java/com/curtisnewbie/agent/perf/PerfAgent.java
@@ -2,14 +2,22 @@ package com.curtisnewbie.agent.perf;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
 
 import java.lang.instrument.Instrumentation;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 /**
- * This has huge performance impact, the benchmark was simply not reliable, unless we specify the method precisely
+ * This may have huge performance impact if too many classes are intercepted,
+ * in such case, the benchmark is simply not reliable.
+ * <p>
+ * We should always specify the type and method precisely.
  *
  * @author yongj.zhuang
  */
@@ -17,45 +25,57 @@ public class PerfAgent {
 
     public static Advice timingAdvice = Advice.to(TimingAdvice.class);
 
+    public static final boolean debug = false; // debug for byte-buddy
+    public static final Set<String> typeNames = new HashSet<>();
+    public static final Set<String> methodNames = new HashSet<>();
+
     public static void premain(String arguments, Instrumentation instrumentation) {
-        new AgentBuilder.Default()
-                .type(not(nameStartsWith("java."))
-                        .and(not(isSynthetic()))
-                        .and(not(nameStartsWith("org.slf4j")))
-                        .and(not(nameStartsWith("net.bytebuddy")))
-                        .and(not(nameStartsWith("org.springframework.util")))
-                        .and(not(nameStartsWith("sun.reflect")))
-                        .and(not(nameStartsWith("com.sun")))
-                        .and(not(nameContains("logging")))
-                        .and(not(nameContains("BeanDefinition")))
-                        .and(not(nameContains("ResolvableType")))
-                        .and(not(nameStartsWith("cn.hutool")))
-                        .and(not(nameStartsWith("org.yaml.snakeyaml")))
-                        .and(not(nameStartsWith("com.fasterxml")))
-                        .and(not(nameStartsWith("org.springframework.core.env")))
-                        .and(not(nameStartsWith("org.springframework.core.convert")))
-                        .and(not(nameStartsWith("org.springframework.boot.context.properties")))
-                )
+        final AgentBuilder.Identified.Extendable t = new AgentBuilder.Default()
+                .type(typeMatcher())
                 .transform((builder, type, classLoader, module) ->
-                        builder.visit(timingAdvice.on(isPublic()
-                                .and(not(isAbstract()))
-                                .and(not(isStatic()))))
-                )
-//                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-                .installOn(instrumentation);
+                        builder.visit(timingAdvice.on(methodMatcher())));
+
+        if (debug) {
+            t.with(AgentBuilder.Listener.StreamWriting.toSystemOut()) // for debugging
+                    .installOn(instrumentation);
+            return;
+        }
+
+        t.installOn(instrumentation);
+    }
+
+    public static ElementMatcher<? super MethodDescription> methodMatcher() {
+        ElementMatcher.Junction<? super MethodDescription> tm = not(isAbstract());
+
+        for (String methodName : methodNames) {
+            tm = tm.and(nameStartsWith(methodName));
+        }
+
+        return tm;
+    }
+
+    public static ElementMatcher<? super TypeDescription> typeMatcher() {
+        ElementMatcher.Junction<? super TypeDescription> tm = not(isSynthetic())
+                .and(not(nameStartsWith("java.")))
+                .and(not(nameStartsWith("net.bytebuddy")))
+                .and(not(nameStartsWith("sun.reflect")))
+                .and(not(nameStartsWith("com.sun")));
+
+        for (String typeName : typeNames) {
+            tm = tm.and(nameStartsWith(typeName));
+        }
+
+        return tm;
     }
 
     public static class TimingAdvice {
-
+        public static final String MAIN = "main";
         public static ThreadLocal<Integer> depth = ThreadLocal.withInitial(() -> 0);
-        public static final long UNIT = 1000_000L;
-        public static final long THRESHOLD = 1_000_000_000L;
-
+        public static final long UNIT = 1_000_000L; // ms to ns
 
         @Advice.OnMethodEnter
         static long enter() {
-            if (!Thread.currentThread().getName().equalsIgnoreCase("main"))
-                return 0L;
+            if (!Thread.currentThread().getName().equalsIgnoreCase(MAIN)) return 0L;
 
             depth.set(depth.get() + 1);
             return System.nanoTime();
@@ -63,21 +83,18 @@ public class PerfAgent {
 
         @Advice.OnMethodExit
         static void exit(@Advice.Enter long time, @Advice.Origin String origin, @Advice.AllArguments() Object[] args) {
-            if (!Thread.currentThread().getName().equalsIgnoreCase("main"))
-                return;
+            if (!Thread.currentThread().getName().equalsIgnoreCase(MAIN)) return;
 
             long took = (System.nanoTime() - time);
-            if (took >= THRESHOLD) {
-                Object[] copy = new Object[args.length];
-                for (int i = 0; i < args.length; i++) {
-                    if (args[i] != null && !(args[i] instanceof Class) && !isPrimitive(args[i].getClass())) {
-                        copy[i] = args[i].getClass().getName();
-                    } else {
-                        copy[i] = args[i];
-                    }
+            Object[] copy = new Object[args.length];
+            for (int i = 0; i < args.length; i++) {
+                if (args[i] != null && !(args[i] instanceof Class) && !isPrimitive(args[i].getClass())) {
+                    copy[i] = args[i].getClass().getName();
+                } else {
+                    copy[i] = args[i];
                 }
-                System.out.printf("Perf, %d, %s %s, %d, ms\n", depth.get(), origin, Arrays.toString(copy), took / UNIT);
             }
+            System.out.printf("Perf, %d, %s %s, %d, ns%s\n", depth.get(), origin, Arrays.toString(copy), took, (took > UNIT ? " " + (took / UNIT) + "ms" : ""));
             depth.set(depth.get() - 1);
         }
 
